b6dc1cd5d532d1314ad1049316c4b992d980e04f
mkarpats <marko_karpats@outlook.com>
2025-07-26 17:05:29 +0300

    Add suspense for home page & set output mode to "standalone"

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import DefaultLayout from "@/components/default-content-layout"
 import { CurrencyConverter } from "@/containers/home-page/currency-converter-chart"
-import { useCallback, useEffect, useState } from "react"
+import { Suspense, useCallback, useEffect, useState } from "react"
 import { Search } from "@/components/ui/search"
 import { useQueryClient } from "@tanstack/react-query"
 import {
@@ -13,8 +13,19 @@ import {
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { SkeletonAreaChart } from "@/components/ui/skeleton-area-chart"
 import { useSearchParamsState } from "@/hooks/useSearchParamsState"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Home() {
+  return (
+    <DefaultLayout>
+      <Suspense fallback={<HomeLoading/>}>
+        <HomeContent/>
+      </Suspense>
+    </DefaultLayout>
+  )
+}
+
+function HomeContent() {
   const [ticker, setTicker] = useSearchParamsState("ticker")
   const [baseCurrency, setBaseCurrency] = useState<string>()
   const [targetCurrency, setTargetCurrency] = useSearchParamsState("target-currency")
@@ -61,35 +72,50 @@ export default function Home() {
   }, [queryClient, ticker])
 
   return (
-    <DefaultLayout>
-      <div className={"flex flex-col gap-2"}>
-        <div className={"flex gap-5"}>
-          <Search
-            className={"flex-1"}
-            fetchItems={searchStocks}
-            placeholder={"Search ticker"}
-            onSelect={res => res && setTicker(res.value)}
-            label={"Ticker"}
-          />
-          <Search
-            className={"flex-1"}
-            fetchItems={searchCurrency}
-            placeholder={"Search currency"}
-            onSelect={res => res && setTargetCurrency(res.value)}
-            label={"Currency"}
-          />
+    <div className={"flex flex-col gap-2"}>
+      <div className={"flex gap-5"}>
+        <Search
+          className={"flex-1"}
+          fetchItems={searchStocks}
+          placeholder={"Search ticker"}
+          onSelect={res => res && setTicker(res.value)}
+          label={"Ticker"}
+        />
+        <Search
+          className={"flex-1"}
+          fetchItems={searchCurrency}
+          placeholder={"Search currency"}
+          onSelect={res => res && setTargetCurrency(res.value)}
+          label={"Currency"}
+        />
 
-        </div>
-        {ticker && targetCurrency && <CurrencyConverter ticker={ticker} targetCurrency={targetCurrency}/> ||
-          <Alert>
-            <AlertTitle>
-              <span className={"text-lg font-bold"}>Please select ticker and target currency for conversion</span>
-            </AlertTitle>
-            <AlertDescription className={"p-5"}>
-              <SkeletonAreaChart/>
-            </AlertDescription>
-          </Alert>}
       </div>
-    </DefaultLayout>
+      {ticker && targetCurrency && <CurrencyConverter ticker={ticker} targetCurrency={targetCurrency}/> ||
+        <Alert>
+          <AlertTitle>
+            <span className={"text-lg font-bold"}>Please select ticker and target currency for conversion</span>
+          </AlertTitle>
+          <AlertDescription className={"p-5"}>
+            <SkeletonAreaChart/>
+          </AlertDescription>
+        </Alert>
+      }
+    </div>
+  )
+}
+
+function HomeLoading() {
+  return (
+    <div className={"flex flex-col gap-2"}>
+      <div className={"flex gap-5"}>
+        <Skeleton className={"flex-1 h-8"}/>
+        <Skeleton className={"flex-1 h-8"}/>
+      </div>
+      <Alert>
+        <AlertDescription className={"p-5"}>
+          <SkeletonAreaChart/>
+        </AlertDescription>
+      </Alert>
+    </div>
   )
 }
